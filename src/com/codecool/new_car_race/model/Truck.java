313a512c8b8699eaf2c1aad7bfaa55d1cf5239c2
Arkadiusz Szreiber <aszreiber1891@gmail.com>
2019-03-30 11:53:13 +0100

    Truck preparation for lap done, tested

--- src/com/codecool/new_car_race/model/Truck.java
+++ src/com/codecool/new_car_race/model/Truck.java
@@ -8,24 +8,52 @@ public class Truck extends Vehicle {
     //private int normalSpeed; // always 100km/h
     //private String name;// Truck drivers call all their trucks a random number between 0 and 1000.
     private int breakdownTurnsLeft;// holds how long its still broken down.
-    private Vehicle vehicle;
     //private int distanceTraveled;// holds the summarized distance traveled in the race.
+    private void chanceOfBreakingDown() {
+        Random random = new Random();
+        int chanceToBreak = random.nextInt(100);
+        if (chanceToBreak < 5) {
+            breakdownTurnsLeft = 2;
+        }
+    }
+
+    public boolean isBroken() {
+        return breakdownTurnsLeft > 0;
+    }
+
+    /**
+     * I decided to break Truck at the end of the lap,
+     * so it not possible for Truck to be broken for more than 2 laps in the row
+     */
+    public void moveForAnHour() {
+        super.moveForAnHour();
+        if (isBroken()) {
+            repairing();
+        } else {
+            chanceOfBreakingDown();
+        }
+    }
+
 
     public void prepareForLap(Race race) {// setup the actual speed used for the current lap
-        if(breakdownTurnsLeft > 0){
+        if (isBroken()) {
             currentSpeed = 0;
         } else {
             currentSpeed = normalSpeed;
         }
     }
 
-
+    private void repairing(){
+        if(isBroken()){
+            breakdownTurnsLeft--;
+        }
+    }
 
     public Truck() {
         Random randomGenerator = new Random();
         int truckNumber = randomGenerator.nextInt(1000) + 1;
         this.name = Integer.toString(truckNumber);
-
-
+        normalSpeed = 100;
+        breakdownTurnsLeft = 0;
     }
 }
--- src/com/codecool/new_car_race/model/TruckTest.java
+++ src/com/codecool/new_car_race/model/TruckTest.java
@@ -15,4 +15,24 @@ class TruckTest {
             assertTrue(truckNumber >= 0 && truckNumber <= 1000);
         }
     }
+
+    @Test
+    void testPreparationIfIsRoadworthy() {
+        Truck truck = new Truck();
+        Race race = new Race();
+        int brokenCounter = 0;
+        int numberOfTests = 100;
+        for (int i = 0; i < numberOfTests; i++) {
+            truck.prepareForLap(race);
+            if (truck.isBroken()) {
+                brokenCounter++;
+                assertEquals(0, truck.getCurrentSpeed());
+            } else {
+                assertEquals(100, truck.getCurrentSpeed());
+            }
+            truck.moveForAnHour();
+        }
+        System.out.println("Truck broken ratio: " + (double) brokenCounter / numberOfTests);
+    }
+
 }
