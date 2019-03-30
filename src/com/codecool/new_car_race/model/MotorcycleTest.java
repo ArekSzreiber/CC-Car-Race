2c252600fdf5f07c31e550a80fe243fa665e50d6
Arkadiusz Szreiber <aszreiber1891@gmail.com>
2019-03-30 06:52:18 +0100

    testing Motocycle preparation in dry weather

--- src/com/codecool/new_car_race/model/MotorcycleTest.java
+++ src/com/codecool/new_car_race/model/MotorcycleTest.java
@@ -6,20 +6,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MotorcycleTest {
     @Test
-    void testNaming(){
+    void testNaming() {
         for (int i = 1; i <= 10; i++) {
             Vehicle motor = new Motorcycle();
-            assertEquals("Motorcycle "+i, motor.getName());
+            assertEquals("Motorcycle " + i, motor.getName());
         }
     }
 
     @Test
-    void testPreparationInRain(){
+    void testPreparationInRain() {
         Race race = new Race(true);
         for (int i = 0; i < 10; i++) {
             Motorcycle motor = new Motorcycle();
             motor.prepareForLap(race);
-            assertTrue(motor.getCurrentSpeed() >= 50 && motor.getCurrentSpeed() <=100);
+            assertTrue(motor.getCurrentSpeed() >= 50 && motor.getCurrentSpeed() <= 100);
         }
     }
+
+    @Test
+    void testPreparationInDryWeather() {
+        Race race = new Race(false);
+        Motorcycle motor = new Motorcycle();
+        motor.prepareForLap(race);
+        assertEquals(100, motor.getCurrentSpeed());
+    }
+
 }
