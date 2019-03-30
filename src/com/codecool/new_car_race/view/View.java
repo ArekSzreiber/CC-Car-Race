effef4b3e969cd6c752374860a29711671891080
Arkadiusz Szreiber <aszreiber1891@gmail.com>
2019-03-30 12:39:55 +0100

    view initial commit; showing vehicles names

---
+++ src/com/codecool/new_car_race/view/View.java
@@ -0,0 +1,14 @@
+package com.codecool.new_car_race.view;
+
+import com.codecool.new_car_race.model.Race;
+import com.codecool.new_car_race.model.Vehicle;
+
+public class View {
+    public static void showVehiclesNames(Race race){
+        Vehicle[] vehicles = race.getVehicles();
+        int i = 1;
+        for(Vehicle vehicle: vehicles){
+            System.out.println(i+++". "+vehicle.getName());
+        }
+    }
+}
